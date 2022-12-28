dc814b5fce91dd9d028db8363f363f5ada676533
Alejandro Arias <alejandroarias@Alejandros-MacBook-Air.local>
2022-12-27 19:00:43 -0600

    navbar setup

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ const Page = () => {
     return (
         <Container> 
             <Box borderRadius="lg" bg = "red" mb={6} p={3} textAlign="center">
-                Hello, I&apos;m an... placeholder
+                Hello, I&apos;m an ambitious full-stack developer based in Austin, Texas!
             </Box>
         <Box display={{md: 'flex'}}>
             <Box flexGrow={1}>
                 <Heading as="h2" variant="page-title">
                     Alejandro Arias Diaz
                 </Heading>
-        <p>Software Engineer</p>
+        <p>Software Engineer Intern @ Amazon</p>
             </Box>
         </Box>
         </Container>
